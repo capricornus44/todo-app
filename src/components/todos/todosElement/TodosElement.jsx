ade137d153f469989a5097b3de554c6107eb5236
capricornus44 <antonzavalniuk@gmail.com>
2021-05-23 12:40:57 +0200

    Code refactoring -> created TodosElement.jsx

---
+++ src/components/todos/todosElement/TodosElement.jsx
@@ -0,0 +1,36 @@
+import React, {useRef} from 'react';
+
+function TodosElement(props) {
+    const {todo, editTodo, completeTodo, deleteTodo} = props;
+    const inputRef = useRef(true)
+
+    const textAreaChangeFocus = () => {
+        inputRef.current.disabled = false;
+        inputRef.current.focus()
+    }
+
+    const edit = (id, value, e) => {
+        if (e.which === 13) { // key code of "Enter"
+            editTodo({id, item: value});
+            inputRef.current.disabled = true
+        }
+    }
+
+    return (
+        <div>
+            <li key={todo.id}>
+                <textarea
+                    ref={inputRef}
+                    disabled={inputRef}
+                    defaultValue={todo.item}
+                    onKeyPress={(e) => edit(todo.id, inputRef.current.value, e)}
+                />
+                <button onClick={() => textAreaChangeFocus()}>Edit</button>
+                <button onClick={() => completeTodo(todo.id)}>Complete</button>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+        </div>
+    );
+}
+
+export default TodosElement;
