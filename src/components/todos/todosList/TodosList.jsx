adba2cb94c5e9db2727fed759dc2cf79b8a6569a
capricornus44 <antonzavalniuk@gmail.com>
2021-05-23 13:21:54 +0200

    Added tasks filter by buttons

--- src/components/todos/todosList/TodosList.jsx
+++ src/components/todos/todosList/TodosList.jsx
@@ -4,14 +4,58 @@ import TodosElement from "../todosElement/TodosElement";
 import {addNewTodo, completeTodo, deleteTodo, editTodo, getFilterTodos} from "../../../redux/store";
 
 function TodosList(props) {
-
+    const [sort, setSort] = useState("active")
     return (
         <div>
-
+            <div>
+                <button onClick={() => setSort("active")}>Active</button>
+                <button onClick={() => setSort("completed")}>Completed</button>
+                <button onClick={() => setSort("all")}>All</button>
+            </div>
             <ul>
-                {props.todos.map(todo => {
-                    return <TodosElement/>
-                })}
+                {props.todos.length > 0 && sort === "active"
+                    ? props.todos.map(todo => {
+                        return (
+                            todo.completed === false &&
+                            <TodosElement
+                                key={todo.id}
+                                todo={todo}
+                                deleteTodo={props.deleteTodo}
+                                editTodo={props.editTodo}
+                                completeTodo={props.completeTodo}
+                            />
+                        )
+                    })
+                    : null}
+
+                {props.todos.length > 0 && sort === "completed"
+                    ? props.todos.map(todo => {
+                        return (
+                            todo.completed === true &&
+                            <TodosElement
+                                key={todo.id}
+                                todo={todo}
+                                deleteTodo={props.deleteTodo}
+                                editTodo={props.editTodo}
+                                completeTodo={props.completeTodo}
+                            />
+                        )
+                    })
+                    : null}
+
+                {props.todos.length > 0 && sort === "all"
+                    ? props.todos.map(todo => {
+                        return (
+                            <TodosElement
+                                key={todo.id}
+                                todo={todo}
+                                deleteTodo={props.deleteTodo}
+                                editTodo={props.editTodo}
+                                completeTodo={props.completeTodo}
+                            />
+                        )
+                    })
+                    : null}
             </ul>
         </div>
     );
