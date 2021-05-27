3d288aeebdc0545c5ee9acd5989ac9ce77b64d7d
capricornus44 <antonzavalniuk@gmail.com>
2021-05-27 19:54:47 +0200

    Added filter of to do

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,45 @@
-import {configureStore, createSlice} from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit"
 
 const todosSlice = createSlice({
-    name: "todos",
-    initialState: {todos: [], filter: ""},
-    reducers: {
-        addNewTodo: (state, action) => ({
-            ...state, todos: [...state.todos, action.payload]
+  name: "todos",
+  initialState: { todos: [], filter: "" },
+  reducers: {
+    addNewTodo: (state, action) => ({
+      ...state,
+      todos: [...state.todos, action.payload],
+    }),
+    deleteTodo: (state, action) => ({
+      ...state,
+      todos: [...state.todos.filter((todo) => todo.id !== action.payload)],
+    }),
+    editTodo: (state, action) => ({
+      ...state,
+      todos: [
+        ...state.todos.map((todo) => {
+          if (todo.id === action.payload.id) {
+            return { ...todo, item: action.payload.item }
+          }
+          return todo
         }),
-        deleteTodo: (state, action) => ({
-            ...state, todos: [...state.todos.filter(todo => todo.id !== action.payload)]
+      ],
+    }),
+    completeTodo: (state, action) => ({
+      ...state,
+      todos: [
+        ...state.todos.map((todo) => {
+          if (todo.id === action.payload) {
+            return { ...todo, completed: true }
+          }
+          return todo
         }),
-        editTodo: (state, action) => ({
-            ...state, todos: [...state.todos.map(todo => {
-                if (todo.id === action.payload.id) {
-                    return {...todo, item: action.payload.item}
-                }
-                return todo
-            })]
-        }),
-        completeTodo: (state, action) => ({
-            ...state, todos: [...state.todos.map(todo => {
-                if (todo.id === action.payload) {
-                    return {...todo, completed: true}
-                }
-                return todo
-            })]
-        }),
-    }
+      ],
+    }),
+    filterTodo: (state, action) => ({ ...state, filter: action.payload }),
+  },
 })
 
-export const {addNewTodo, deleteTodo, editTodo, completeTodo} = todosSlice.actions;
+export const { addNewTodo, deleteTodo, editTodo, completeTodo, filterTodo } = todosSlice.actions
 
 const store = configureStore(todosSlice)
 
-export default store;
+export default store
